b66dfbd3690fc411a419cd3850b156c031a57507
Kozlik19 <aroslavzuravlev6@gmail.com>
2025-01-16 15:39:24 +0000

    Добавили рубокоп

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,12 @@ class User < ApplicationRecord
   validates :email, presence: true
 
   def abcd
-		self.update(password: self.password.split('').shuffle.join)
+    update(password: password.chars.shuffle.join)
   end
 
-	def self.qwer(user)
-		new_user = user.dup.attributes.slice('email', 'password')
-		saved = User.create(new_user)
-		saved.abcd
-	end
+  def self.qwer(user)
+    new_user = user.dup.attributes.slice('email', 'password')
+    saved = User.create(new_user)
+    saved.abcd
+  end
 end
--- db/migrate/20241121142418_create_user.rb
+++ db/migrate/20241121142418_create_user.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Создание пользователя
 class CreateUser < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
