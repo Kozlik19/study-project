6641e0d604529b710ac3eb8e954818b47a335df4
Kozlik19 <aroslavzuravlev6@gmail.com>
2025-02-27 14:58:43 +0000

    Добавили артикли

---
+++ spec/models/article_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Добавление помошника рельс
+require 'rails_helper'
+
+RSpec.describe Article, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
