0e33172f9ca9977898551a311f881a768f306ad5
Yaroslav <aroslavzuravlev6@gmail.com>
2025-02-27 17:59:23 +0300

    Добавили артикли (#7)

---
+++ db/migrate/20250227142503_create_articles.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# Добавление статьи
+class CreateArticles < ActiveRecord::Migration[7.1]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
