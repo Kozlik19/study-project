890d9930c19ffde0b922f728a22b24c396414574
Kozlik19 <aroslavzuravlev6@gmail.com>
2025-03-06 15:17:48 +0000

    Решил несколько ошибок

--- db/migrate/20250306140823_add_status_to_articles.rb
+++ db/migrate/20250306140823_add_status_to_articles.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Добавление статуса для статей
+# Добавление статуса статьям
 class AddStatusToArticles < ActiveRecord::Migration[7.1]
   def change
     add_column :articles, :status, :string
